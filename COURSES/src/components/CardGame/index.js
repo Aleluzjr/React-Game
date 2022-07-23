9b77c25d771191cd381db383f6185f4c3de23a65
Alexandre M Luz <amsljr@hotmail.com>
2022-07-22 23:52:13 -0300

    Styles

---
+++ COURSES/src/components/CardGame/index.js
@@ -0,0 +1,10 @@
+import './styles.css'
+function CardGame (){
+    return `
+        <article class = "card-game">
+            <img src ="images/alura-pixel.png" alt = "Logo ALura">
+        </article>    
+    `
+}
+
+export default CardGame
